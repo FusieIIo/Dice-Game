fef10c7fa50d2e393804b579cb9603bf6ec5716c
Silvio Fusiello <sfusiello@outlook.com>
2020-04-22 11:37:21 +0100

    Add files via upload
    Added query string function

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dice import roll_die
 
 app = Flask(__name__)
 
 
-@app.route("/")
-@app.route("/dice_game")
+@app.route('/')
+@app.route('/dice_game')
 def dice_game():
-    return render_template('dice.html', result=roll_die(), result2=roll_die())
+
+    return render_template('dice.html', result1=request.args.get('result1', type=int) or roll_die(), result2=roll_die())
 
 
 if __name__ == '__main__':
