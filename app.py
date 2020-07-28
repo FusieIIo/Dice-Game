29888a29e57f6830013a2d7ce92a311f4f313c4e
Silvio Fusiello <sfusiello@outlook.com>
2020-07-28 15:12:38 +0100

    Actioned to resolve comments
    Reviewed Pep 8 and updated coding to reflect this.

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask
+
+from flask import render_template
+
+from flask import request
+
 from dice import roll_die
 
 app = Flask(__name__)
 
 
-@app.route('/')
 @app.route('/dice_game')
 def dice_game():
 
-    return render_template('dice.html', result1=request.args.get('result1', type=int) or roll_die(), result2=roll_die())
+    return render_template(
+
+        'dice.html',
+
+        result1=request.args.get('result1', type=int) or roll_die(),
+
+        result2=roll_die())
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
